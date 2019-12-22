2c3188cc2c06f891e1e314ab70440b80202f0042
FabienM <fabien.manson@free.fr>
2019-12-22 15:23:44 +0100

    testing graph => Not working

--- graphe.py
+++ graphe.py
@@ -1,5 +1,6 @@
 import math
-import networkx as nx
+#import networkx as nx
+from sagemath import *
 import matplotlib.pyplot as plt
 import random
 
@@ -66,4 +67,10 @@ def circular():
 
 if __name__ == '__main__':
 
-	draw_graphe("hsfkjsfhe", "hsfjksh")
+	#draw_graphe("hsfkjsfhe", "hsfjksh")
+
+	#http://doc.sagemath.org/html/en/reference/graphs/sage/graphs/graph.html#graph-format
+
+	d = {0: [1,4,5], 1: [2,6], 2: [3,7], 3: [4,8], 4: [9], 5: [7, 8], 6: [8,9], 7: [9]}
+	G = Graph(d)
+	G.plot().show()

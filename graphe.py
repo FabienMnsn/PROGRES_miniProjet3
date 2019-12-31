2b9b40a00477aeb904b8c8b38ab600ff91b966b3
FabienMnsn <fabien.manson@free.fr>
2019-12-31 14:24:02 +0100

    graph 2 auteurs 100% working

--- graphe.py
+++ graphe.py
@@ -44,11 +44,12 @@ def get_links(lip6_members_path):
 	return links
 
 
-def draw_graph(lip6_members_path, links_dico, author_name1, author_name2):
+def draw_graph_2membres(lip6_members_path, links_dico, author_name1, author_name2):
 	"""
 	Retourne un graphe reseau des publications entre membres permanents du lip6
 
 	@param
+	lip6_members_path : string, chemin d'acces du fihcer xml des membres permanents du lip6 
 	links_dico : dictionnaire des lien entre chaque membre permanent du lip6
 	author_name1 : string nom de l'auteur1 ex : "Prénom Nom"
 	author_name2 : string nom de l'auteur2 ex : "Prénom Nom"
@@ -58,24 +59,78 @@ def draw_graph(lip6_members_path, links_dico, author_name1, author_name2):
 		G = nx.Graph()
 		color = []
 		node_labels = {}
+		nodes_size = []
+		edges = []
+		edges_color = []
+		list_coaut1 = links_dico[author_name1]
+		list_coaut2 = links_dico[author_name2]
+		#print(list_coaut1)
+		#print(list_coaut2)
 		for key in links_dico.keys():
 			if(key == author_name1):
 				node_labels[author_name1] = author_name1.replace(' ', '\n')
+				nodes_size.append(400)
 				color.append("red")
 			elif(key == author_name2):
 				node_labels[author_name2] = author_name2.replace(' ', '\n')
 				color.append("orange")
+				nodes_size.append(400)
+			elif(key in list_coaut1):
+				color.append("red")
+				node_labels[key] = key.replace(' ', '\n')
+				nodes_size.append(100)
+			elif(key in list_coaut2):
+				color.append("orange")
+				node_labels[key] = key.replace(' ', '\n')
+				nodes_size.append(100)
 			else:
-				color.append("blue")
+				color.append("grey")
+				nodes_size.append(15)
 			G.add_node(key)
-		plt.figure(figsize=(10,10))
-		nx.draw(G, with_labels=True, labels=node_labels, node_color=color, font_size=7)
-		plt.show()
+
+		for elem in list_coaut1:
+			edges.append((author_name1, elem))
+			edges_color.append("red")
+
+		for elem in list_coaut2:
+			edges.append((author_name2, elem))
+			edges_color.append("orange")
+
+		G.add_edges_from(edges)
+		plt.figure(figsize=(15,8))
+		nx.draw_random(G, node_size=nodes_size, with_labels=True, labels=node_labels, node_color=color, edge_color=edges_color, font_size=8, font_weight='bold')
+		plt.savefig("graphe2.png", dpi=200)
+		#plt.show()
 	else:
 		print("ERROR : incorrect name authors")
 
+
+def draw_graph_all(links_dico):
+	"""
+	Retourne rien mais enregistre une image du schéma des publication entre tous les membres permanents du lip6
+	
+	@param
+	links_dico : dictionnaire des lien entre chaque membre permanent du lip6
+	"""
+	G = nx.Graph()
+	color = []
+	node_labels = {}
+	edges = []
+	for elem in links_dico:
+		node_labels[elem] = elem.replace(' ', '\n')
+		for each in links_dico[elem]:
+			edges.append((elem,each))
+
+	G.add_edges_from(edges)
+	plt.figure(figsize=(15,8))
+	nx.draw_random(G, with_labels=True, node_size=20, labels=node_labels, node_color="blue", font_size=6, font_weight='bold')
+	plt.savefig("grapheAll.png", dpi=200)
+
+
 if __name__ == '__main__':
 
-	draw_graph("Auteurs/lip6.xml", get_links("Auteurs/lip6.xml"), "Olivier Fourmaux", "Julien Sopena")
+
+	draw_graph_2membres("Auteurs/lip6.xml", get_links("Auteurs/lip6.xml"), "Swan Dubois", "Sébastien Tixeuil")
+	#draw_graph_all(get_links("Auteurs/lip6.xml"))
 	#print(liste_lip6("Auteurs/lip6.xml"))
 	#http://doc.sagemath.org/html/en/reference/graphs/sage/graphs/graph.html#graph-format

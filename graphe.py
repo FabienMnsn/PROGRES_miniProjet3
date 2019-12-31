cc9ab97065bfdf7aa87ef0690ce7378fdaad80e5
FabienMnsn <fabien.manson@free.fr>
2019-12-31 13:33:43 +0100

    update graphe

--- graphe.py
+++ graphe.py
@@ -1,64 +1,12 @@
 #---------------------------
 import utils_xml
 #---------------------------
-import math
 import os
-import xml.etree.ElementTree as ET
-#import networkx as nx
+import networkx as nx
 #from sagemath import *
 #from sage.all import *
-#import matplotlib.pyplot as plt
-#import random
+import matplotlib.pyplot as plt
 
-
-
-
-
-def list_all_publication(author_name1, author_name2):
-	"""
-	Retourne un dictionnaire contenant les lien de publication entre tous les membres permanents du lip6
-
-	@param
-	author_name1 : string, nom de l'auteur 1 ex:"Sébastien Tixeuil"
-	author_name2 : string, nom de l'auteur 2 ex:"Olivier Fourmaux"
-	"""
-	links = {}
-	found = 0 
-	lip6 = utils_xml.liste_lip6("Auteurs/lip6.xml")
-	for i in lip6:
-		if(i == author_name1 or i == author_name2):
-			found += 1
-		else:
-			continue
-	if(found != 2):
-		print("Un des deux auteur n'est pas bien orthographié ou n'est pas un membre permanent du lip6")
-		return -1
-	else:
-		list_file = os.listdir("Auteurs/")
-		if(author_name1+".xml" not in list_file):
-			print("telechargement du fichier de :", author_name1)
-			utils_xml.download_file(author_name1, "Auteurs/", "table_html.txt")
-		if(author_name2+".xml" not in list_file):
-			print("telechargement du fichier de :", author_name2)
-			utils_xml.download_file(author_name2, "Auteurs/", "table_html.txt")
-		
-		l1_coauteurs = utils_xml.get_coauteurs("Auteurs/"+author_name1+".xml")
-		l2_coauteurs = utils_xml.get_coauteurs("Auteurs/"+author_name2+".xml")
-		
-		L1 = []
-		for aut in l1_coauteurs:
-			if(aut in lip6):
-				L1.append(aut)
-		links[author_name1] = L1
-
-		L2 = []
-		for aut in l2_coauteurs:
-			if(aut in lip6):
-				L2.append(aut)
-		links[author_name2] = L2
-
-		#print(l1_coauteurs)
-		print(links)
 
 
 def get_links(lip6_members_path):
@@ -91,20 +39,43 @@ def get_links(lip6_members_path):
 	#for elem in links:
 		#i += 1
 		#print(i, elem, links[elem])
-	print("Impossible de récupérer", echec, "/", len(lip6), "fichiers (dead links in dblp)")
+	#print(links.keys())
+	#print("Impossible de récupérer", echec, "/", len(lip6), "fichiers (dead links in dblp)")
+	return links
 
 
-def draw_graph(links_dico):
+def draw_graph(lip6_members_path, links_dico, author_name1, author_name2):
 	"""
 	Retourne un graphe reseau des publications entre membres permanents du lip6
 
 	@param
 	links_dico : dictionnaire des lien entre chaque membre permanent du lip6
+	author_name1 : string nom de l'auteur1 ex : "Prénom Nom"
+	author_name2 : string nom de l'auteur2 ex : "Prénom Nom"
 	"""
-
+	lip6 = utils_xml.liste_lip6(lip6_members_path)
+	if(author_name1 in lip6 and author_name2 in lip6):
+		G = nx.Graph()
+		color = []
+		node_labels = {}
+		for key in links_dico.keys():
+			if(key == author_name1):
+				node_labels[author_name1] = author_name1.replace(' ', '\n')
+				color.append("red")
+			elif(key == author_name2):
+				node_labels[author_name2] = author_name2.replace(' ', '\n')
+				color.append("orange")
+			else:
+				color.append("blue")
+			G.add_node(key)
+		plt.figure(figsize=(10,10))
+		nx.draw(G, with_labels=True, labels=node_labels, node_color=color, font_size=7)
+		plt.show()
+	else:
+		print("ERROR : incorrect name authors")
 
 if __name__ == '__main__':
 
-	get_links("Auteurs/lip6.xml")
+	draw_graph("Auteurs/lip6.xml", get_links("Auteurs/lip6.xml"), "Olivier Fourmaux", "Julien Sopena")
 	#print(liste_lip6("Auteurs/lip6.xml"))
 	#http://doc.sagemath.org/html/en/reference/graphs/sage/graphs/graph.html#graph-format
